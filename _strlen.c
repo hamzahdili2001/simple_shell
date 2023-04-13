0670c21075c5c288686cb8d4d9b19d7dc634dcca
hamzahdili2001 <hdilihamza955@gmail.com>
2023-04-13 00:43:35 +0000

    Fix: Merge conflict

---
+++ _strlen.c
@@ -0,0 +1,12 @@
+
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+		len++;
+
+	return (len);
+}
+
--- shell.h
+++ shell.h
@@ -2,4 +2,8 @@
 #define SHELL_H
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+void main_loop(void);
+int _strlen(char *str);
+char *read_line(void);
 #endif
