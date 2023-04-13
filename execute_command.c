2ecfbc60abbe018b4e5c366a3a9671414dbc0a8e
hamzahdili2001 <hdilihamza955@gmail.com>
2023-04-13 22:48:54 +0000

    add: in execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,14 +1,30 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void execute_command(char **args)
 {
 	pid_t pid;
 	int status;
+	/*ADD CONCAT*/
+	char *concat = malloc(_strlen(args[0]) + _strlen("/bin/") + 1);
+
+	/*CHECK MALLOC*/
+	if (concat == NULL)
+	{
+		perror("Allocation faild");
+		exit(EXIT_FAILURE);
+	}
+
+	/*Concatenate input with "/bin/"*/ 
+	strcpy(concat, "/bin/");
+	strcat(concat, args[0]);
 
 	pid = fork();
 	if (pid == 0)
 	{
-		if (execve(args[0], args, NULL) == -1)
+		if (execve(concat, args, NULL) == -1)
 		{
 			perror("execve");
 			exit(EXIT_FAILURE);
