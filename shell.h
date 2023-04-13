09cc45638229e94e1ebd751c9a07769d8232b29a
hamzahdili2001 <hdilihamza955@gmail.com>
2023-04-13 23:24:57 +0000

    remove: dublicat strlen edit: strlen in string_helpers.c

--- shell.h
+++ shell.h
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
+#include <string.h>
 #define TOKEN_DELIM " \t\r\n\a"
 #define BUFFER_SIZE 1024
 void main_loop(void);
@@ -13,7 +13,6 @@ int _strlen(char *str);
 char *read_line(void);
 void execute_command(char **args);
 char **parse_line(char *line);
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 #endif
