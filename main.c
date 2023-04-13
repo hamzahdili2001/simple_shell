0903935cc8b5219711b6ede3112b14c77b0b1b7a
Brexblime <jaouhari.bahaadine@gmail.com>
2023-04-13 00:38:58 +0000

    read_line file

---
+++ main.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+#include <stdio.h>
+
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t buffer_size = 0;
+
+	if (getline(&line, &buffer_size, stdin) == -1)
+	{
+			perror("getline");
+			exit(EXIT_FAILURE);
+	}
+	printf("%s", line);
+	return (line);
+}
+
+void main_loop(void)
+{
+	char *prompt = "$ ", *line;
+	size_t line_length;
+	int status = 1;
+
+	do {
+		printf("%s", prompt);
+		line = read_line();
+	} while (status);
+
+}
+
+int main (void)
+{
+	main_loop();
+}
+
---
+++ read_line.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t buffer_size = 0;
+
+	if (getline(&line, &buffer_size, stdin) == -1)
+	{
+		perror("getline");
+		exit(EXIT_FAILURE);
+	}
+	write(STDOUT_FILENO, line, buffer_size);
+	return (line);
+}
